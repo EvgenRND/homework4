7e44bf4e1cff72202067c17909b759729944364e
EvgenRND <dertion8@gmail.com>
2024-12-11 23:20:30 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,5 @@
+number_of_completed_tasks=12
+number_of_hours=1.5
+Course_name= 'Python'
+Time_for_one_task=number_of_hours/number_of_completed_tasks
+print('Курс:',Course_name+",",'всего задач:',number_of_completed_tasks,',','затрачено часов:',number_of_hours,',','среднее время выполнения',Time_for_one_task,'часа.')
